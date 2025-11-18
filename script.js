91d3e85bc91de6799cfd9239a991623bf421506b
cflatminor <cflatminor92@gmail.com>
2025-11-18 14:17:16 +0100

    new part

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ $(document).ready(function () {
     let tasks = loadTasks();
     render();
     renderDate();
-});
+
+    function renderDate() {
+        const days = ["Воскресенье","Понедельник","Вторник","Среда","Четверг","Пятница","Суббота"];
+        const months = ["января","февраля","марта","апреля","мая","июня","июля","августа","сентября","октября","ноября","декабря"];
+        const d = new Date();
+        $("#day").text(`${days[d.getDay()]}, ${d.getDate()} ${months[d.getMonth()]}`);
+    }
+
+    
